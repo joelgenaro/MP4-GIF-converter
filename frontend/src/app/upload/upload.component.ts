62d3752f277fb1bf0dd886bf8fcf7c2ec7694a2c
Joel Genaro <joel.ggdev@gmail.com>
2024-10-23 20:13:05 +0900

    video.onloadedmetadata = () => {

--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -12,11 +12,31 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class UploadComponent {
   private selectedFile: File | null = null;
   gifUrl: string | null = null;
+  errorMessage: string | null = null;
+
+  @ViewChild('videoElement', { static: false })
+  videoElement!: ElementRef<HTMLVideoElement>;
 
   constructor(private http: HttpClient) { }
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
+    this.errorMessage = null;
+
+    if (this.selectedFile) {
+      const video = this.videoElement.nativeElement;
+      video.src = URL.createObjectURL(this.selectedFile);
+
+      video.onloadedmetadata = () => {
+        if (video.videoWidth > 1024 || video.videoHeight > 768) {
+          this.errorMessage = 'Video dimensions should not exceed 1024x768.';
+          this.selectedFile = null;
+        } else if (video.duration > 10) {
+          this.errorMessage = 'Video duration should not exceed 10 seconds.';
+          this.selectedFile = null;
+        }
+      };
+    }
   }
 
   onUpload() {
@@ -38,7 +58,7 @@ export class UploadComponent {
           document.body.removeChild(a);
         });
     } else {
-      alert('No file selected');
+      alert('No file selected or file does not meet the requirements.');
     }
   }
 }
